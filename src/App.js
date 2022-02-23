9f6c50783fe2463e8e13d8f3b6b3c2d92e88674c
Jonathan Shoemaker <syco1316@gmail.com>
2022-02-23 16:38:30 -0600

    2nd commit setting up Product diplays

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
-import Header from "./containers/Header"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from "./containers/Header";
+import ProductListing from './containers/ProductListing';
+import ProductDetail from './containers/ProductDetail';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 Not Found</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+//in case you struggling with react-router-dom, they had updates, so you need to do it like that :
+
+{/* <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/productlist' element={<ProductList />}/>
+        </Routes>
+      </BrowserRouter>
+    </div> */}
